e0234aa6de76f79b8fc7ec83c9c66817cb9b6215
Selma434 <62303684+Selma434@users.noreply.github.com>
2022-03-08 11:45:16 +0000

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -9,16 +9,16 @@ sudo apt install python3 python3-venv python3-pip -y
 
 
 
-#install docker
-if [ ! -f "/usr/bin/docker" ]; then
-    curl https://get.docker.com | sudo bash
-    sudo usermod -aG docker jenkins
-fi
+# #install docker
+# if [ ! -f "/usr/bin/docker" ]; then
+#     curl https://get.docker.com | sudo bash
+#     sudo usermod -aG docker jenkins
+# fi
 
-#install docker compose
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# #install docker compose
+# version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # docker login
 docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
