5a6d491ae7418b5adf1050c022cbfd01bfb63fa0
Ubuntu <selmayussuf@selma-fp-vm.bakezzseo3iudex20olftsqy4b.zx.internal.cloudapp.net>
2022-03-10 13:31:48 +0000

    adding env variables

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -9,4 +9,6 @@ scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm-manager \
     DATABASE_URI=$DATABASE_URI \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+    MYSQL_DATABASE=$MYSQL_DATABASE \
     docker stack deploy --compose-file docker-compose.yaml final-project
